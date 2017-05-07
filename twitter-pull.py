613baa2c63b92d6d7438bd77984bf3a1501959cc
3lim <elim92@gmail.com>
2017-05-06 18:22:27 -0700

    Repeatedly streams randomly selected stocks.

--- twitter-pull.py
+++ twitter-pull.py
@@ -1,4 +1,5 @@
-import twitter, yaml, os.path, sys, json
+import twitter, yaml, os.path, sys, json, random
+from time import gmtime, strftime, time
 
 def read_config():
     config = None
@@ -14,18 +15,40 @@ def load_twitter(config):
                   access_token_secret=twitter_config["access_token_secret"])
     return twitter_api
 
+# each line in the file is a tracked word.
+def load_tracked_words(filename):
+    with open(filename, 'r') as f:
+        content = f.readlines()
+        # remove whitespace at beginning and end of each line.
+        content = [x.strip() for x in content]
+        return content
+    return None
+
+def record_tweet_stream(twitter_api, tracked_words, output_file, time_limit_minutes=30):
+    track_languages = ['en']
+    time_start = time()
+
+    with open(output_file, 'w',encoding="utf-8") as f:
+        for line in twitter_api.GetStreamFilter(track=tracked_words, languages=track_languages):
+            f.write(json.dumps(line))
+            f.write('\n')
+            if time() - time_start > time_limit_minutes * 60:
+                break
+
 def main():
     config = read_config()
     twitter_api = load_twitter(config)
 
-    track_languages = ['en']
-    track_stocks = ['Google']
-
-    for line in twitter_api.GetStreamFilter(track=track_stocks, languages=track_languages):
-        #tweet = json.loads(line)
-        print(line['text'].encode("utf-8"))
+    # pick 200 randomly sampled words and stream for 30 minutes per sample endlessly.
+    while True:
+        tracked_words = random.sample(load_tracked_words(config["twitter"]["input_file"]), 200)
+        print(tracked_words)
         print('\n')
 
+        output_filename = str.format("tweet_stream_{}.json",strftime("%Y%m%d_%H%M%S",gmtime()))
+        output_file = os.path.join(config["twitter"]["output_dir"],output_filename)
+
+        record_tweet_stream(twitter_api, tracked_words, output_file)
 
 if __name__ == '__main__':
     main()
